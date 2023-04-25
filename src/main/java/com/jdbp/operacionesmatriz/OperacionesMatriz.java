9ea832f56af36247e4886654a5882bd3cefd57fb
JuanDa-B <beltranpiza4@gmail.com>
2023-04-24 21:20:43 -0500

    first commit

---
+++ src/main/java/com/jdbp/operacionesmatriz/OperacionesMatriz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jdbp.operacionesmatriz;
+
+/**
+ *
+ * @author JuanDa
+ */
+public class OperacionesMatriz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
